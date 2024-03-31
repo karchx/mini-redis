fa12c0c40879a309441acd998a0ac6a62a448662
karchx <kenethra.74@gmail.com>
2024-03-30 18:18:56 -0600

    feat: multi ping

--- src/main.rs
+++ src/main.rs
@@ -1,13 +1,37 @@
-use std::{io::Write, net::TcpListener};
+use std::{
+    io::{BufRead, BufReader, Write},
+    net::{TcpListener, TcpStream},
+};
+
+fn handle_incoming_connection(mut stream: TcpStream) {
+    let read_stream = stream.try_clone().expect("Stream should be clonable");
+    let reader = BufReader::new(read_stream);
+
+    for line in reader.lines() {
+        match line {
+            Ok(command) => {
+                println!("Received {}", command);
+                if command == "ping" {
+                    stream
+                        .write_all("+PONG\r\n".as_bytes())
+                        .expect("data written");
+                    println!("data sent");
+                }
+            }
+            Err(e) => {
+                println!("Failure {}", e);
+                break;
+            }
+        }
+    }
+}
 
 fn main() {
     let listener = TcpListener::bind("127.0.0.1:6379").unwrap();
 
     for stream in listener.incoming() {
         match stream {
-            Ok(mut stream) => stream
-                .write_all("+PONG\r\n".as_bytes())
-                .expect("data written"),
+            Ok(stream) => handle_incoming_connection(stream),
             Err(e) => {
                 println!("error: {}", e);
             }
